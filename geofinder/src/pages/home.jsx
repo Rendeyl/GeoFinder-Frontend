aef0bce204e64f9a9fe1cc202610e9464a8e136c
RenDeyl <rendey73@gmail.com>
2025-08-30 23:38:43 +0800

    v1

--- geofinder/src/pages/home.jsx
+++ geofinder/src/pages/home.jsx
@@ -1,4 +1,9 @@
+
+
 function Home(){
+
+    const current_ip = "1.2.34.52";
+
     return(
         <>
         <div id="upper">
@@ -6,6 +11,16 @@ function Home(){
             <input type="text" placeholder="Search IP" id="searchBar"/>
             <button className="upperBtn">Search</button>
             <button className="upperBtn">Clear</button>
+        </div>
+
+        <div id="lower">
+            <div id="info">
+                <div id="info-box1">
+                    <h1 className="infoText">IP:&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;{current_ip}</h1>
+                </div>
+            </div>
+
+            <div id="history"></div>
         </div>
         </>
     );
