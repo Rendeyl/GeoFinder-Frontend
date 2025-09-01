ecff63246c945a9a76900646770cfb3bced5fa1d
RenDeyl <rendey73@gmail.com>
2025-09-01 14:03:50 +0800

    v1.4

--- geofinder/src/pages/home.jsx
+++ geofinder/src/pages/home.jsx
@@ -1,7 +1,9 @@
 import { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
 
 function Home(){
     const [info, setInfo] = useState(null);
+    const [search, setSearch] = useState("");
 
     useEffect(() =>{
         fetch("https://ipinfo.io/json")
@@ -12,13 +14,66 @@ function Home(){
         .catch((err) => console.error(err));
     }, []);
 
+    async function searchIP() {
+        try{
+            fetch(`https://ipinfo.io/${search}/geo`)
+            .then((res) => res.json())
+            .then((data) => {
+            setInfo(data);
+        })
+        }catch (err){
+            console.log(err);
+        }
+    }
+
+    async function clearIP() {
+        try{
+            fetch(`https://ipinfo.io/json`)
+            .then((res) => res.json())
+            .then((data) => {
+            setInfo(data);
+            setSearch("");
+        })
+        }catch (err){
+            console.log(err);
+        }
+    }
+
+    async function call() {
+        try{
+            const res = await fetch("https://geofinder-api.vercel.app/api/login",
+                {
+                    method: "POST",
+                    headers: {
+                        "Content-Type": "application/json"
+                    },
+                    body: JSON.stringify({ username, password }),
+                }
+            );
+
+            const data = await res.json();
+
+            if(res.ok){
+                console.log(data.message);
+            }else{
+                alert("error");
+            }
+
+        }catch (err){
+            console.log(err);
+            alert("error");
+        }
+    }
+
     return(
         <>
         <div id="upper">
+            <Link to={"/"}>
             <h1 id="geoFinder2">GeoFinder</h1>
-            <input type="text" placeholder="Search IP" id="searchBar"/>
-            <button className="upperBtn">Search</button>
-            <button className="upperBtn">Clear</button>
+            </Link>
+            <input type="text" placeholder="Search IP" id="searchBar" value={search} onChange={(e) => setSearch(e.target.value)}/>
+            <button className="upperBtn" onClick={searchIP}>Search</button>
+            <button className="upperBtn" onClick={call}>Clear</button>
         </div>
 
         <div id="lower">
@@ -34,7 +89,7 @@ function Home(){
                     <h1 className="infoText">Timezone: &nbsp; {info?.timezone}</h1>
                 </div>
                 <div id="info-box3">
-                    <h1 className="infoText">Org: {info?.org} </h1>
+                    <h1 className="infoText">Org: &nbsp; {info?.org} </h1>
                 </div>
             </div>
 
