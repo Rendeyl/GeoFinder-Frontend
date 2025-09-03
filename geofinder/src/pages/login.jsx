ce57c7a05c3df3e4e857ff2135f87cb57ea33ac3
RenDeyl <rendey73@gmail.com>
2025-09-03 23:15:39 +0800

    v2.4.3

--- geofinder/src/pages/login.jsx
+++ geofinder/src/pages/login.jsx
@@ -25,7 +25,6 @@ function Login(){
 
             if(res.ok){
                 console.log(data.message);
-                localStorage.setItem("token", data.token);
                 navigate("/home");
             }else{
                 alert("Invalid Login");
